789fc41fc7d8451eb61462df4f08d2e3ac4ecb9d
seongdong3 <seongdong.3.nodecrew@gmail.com>
2025-09-17 13:40:16 +0900

    20250917 연산자

--- js/04-operator/main.js
+++ js/04-operator/main.js
@@ -10,7 +10,6 @@ const falseData = false;
 // 템플릿 리터럴에는 변수뿐만 아니라 표현식을 삽입 가능
 // 표현식 : 데이터를 생성하는 코드
 console.log(`true && true -> ${trueData && trueData}`);
-
 // false && true 출력(log)
 console.log(`false && true -> ${falseData && trueData}`);
 
@@ -26,3 +25,20 @@ console.log(`false || false -> ${falseData || falseData}`);
 // 논리형 데이터가 참이면 거짓(false) 생성
 console.log(`!true -> ${!true}`);
 console.log(`!false -> ${!false}`);
+
+// 같다 == Vs. === / 다르다 != Vs. !==
+// == / !=
+// 동등 비교
+// 값(데이터)만 비교
+// 서로 다른 자료형을 비교하면 자료형을 동일하게 변환
+// 절대 절대 사용 금지
+const number = 1;
+const str = "1";
+const bool1 = number == str;
+console.log(bool1); // true
+
+// === / !==
+// 동등 비교
+// 데이터와 데이터의 자료형을 비교
+const bool2 = number === str;
+console.log(bool2); // false
