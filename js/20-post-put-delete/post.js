2b1226975595bf6ed337c7a30a55983c292ef30c
seongdong3 <seongdong.3.nodecrew@gmail.com>
2025-09-25 15:39:18 +0900

    post-put-delete

---
+++ js/20-post-put-delete/post.js
@@ -0,0 +1,41 @@
+// 20-post-put-delete/post.js
+
+import axios from "axios";
+
+async function addProduct() {
+  // 요청 주소
+  const URL = "https://dummyjson.com/products/add";
+  // 요청 방법(목적)
+  const METHOD = "POST";
+  // 요청 본문(body)
+  const body = {
+    title: "갤럭시 s999",
+    category: "스마트폰",
+  };
+  // 요청 헤더(headers)
+  // 헤더(headers) : 요청의 부가 정보
+  const headers = {
+    // Content-Type : 요청 본문(body)의 데이터 형식
+    // application/json : json 확장자 데이터
+    "Content-Type": "application/json",
+  };
+
+  // axios 요청 설정 파일
+  const config = {
+    url: URL,
+    method: METHOD,
+    headers: headers,
+    // data: axios에서의 본문(body)
+    data: body,
+  };
+  // axios로 API 요청
+  const response = await axios(config);
+
+  // 데이터(data) 객체 추출
+  const data = response["data"];
+
+  console.log(data);
+}
+
+// 함수 호출
+addProduct();
---
+++ js/20-post-put-delete/put.js
@@ -0,0 +1,23 @@
+// put.js
+
+import axios from "axios";
+async function putProduct() {
+  // axios 설정 객체 config
+  const config = {
+    url: "https://dummyjson.com/products/1",
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    data: {
+      title: "아이폰 999",
+    },
+  };
+  // 응답 데이터 저장
+  const response = await axios(config);
+
+  // 응답 데이터에서 data 속성 추출
+  const data = response["data"];
+
+  console.log(data);
+}
+// 함수 호출
+putProduct();
