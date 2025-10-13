3a362c3aa5990b80b7310d03cb191d7f66ade5ad
seongdong3 <seongdong.3.nodecrew@gmail.com>
2025-10-13 14:52:08 +0900

    중첩 라우팅

--- react/react-router-app/src/router/index.js
+++ react/react-router-app/src/router/index.js
@@ -1,33 +1,64 @@
-// 페이지 컴포넌트 불러오기
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Profile from "../pages/Profile";
-import Login from "../pages/Login";
 // React Router의 createBrowserRouter 불러오기
 // createBrowserRouter 함수
 // 라우터 설정을 생성하는 함수
 // 라우터 : 주소(URL)와 컴포넌트를 매핑
 import { createBrowserRouter } from "react-router-dom";
 
+// 레이아웃 컴포넌트 불러오기
+import RootLayout from "../layout/RootLayout";
+import AuthLayout from "../layout/AuthLayout.jsx";
+
+// 페이지 컴포넌트 불러오기
+import Home from "../pages/RootPages/Home.jsx";
+import About from "../pages/RootPages/About.jsx";
+import Profile from "../pages/RootPages/Profile.jsx";
+
+import AuthHome from "../pages/AuthPages/AuthHome.jsx";
+import Signup from "../pages/AuthPages/Signup.jsx";
+import Login from "../pages/AUthPages/Login.jsx";
+
 // 라우터 설정 생성
 const router = createBrowserRouter([
   {
-    path: "/", // 주소(URL)
-    Component: Home, // 주소에 해당되는 컴포넌트
+    path: "/",
+    Component: RootLayout,
+    children: [
+      // 중첩할 자식 경로 객체를 정의하는 배열
+      {
+        index: true, // index: true -> 부모 경로의 기본(root) 경로
+        // 완성되는 path : "/" (부모 경로와 동일)
+        Component: Home,
+      },
+      {
+        path: "about",
+        Component: About,
+      },
+      {
+        path: "profile",
+        Component: Profile,
+      },
+    ],
   },
+  // AuthLayout 경로 설정
   {
-    path: "/about",
-    Component: About, // Component 속성과 동일한 기능을 하지만 컴포넌트 이름이 아니라 컴포넌트 태그를 설정
-  },
-  // "/profile" 로 접근하면
-  // Profile 컴포넌트를 표시하는 속성 작성
-  {
-    path: "/profile",
-    Component: Profile,
-  },
-  {
-    path: "/login",
-    Component: Login,
+    path: "/auth",
+    Component: AuthLayout,
+    children: [
+      {
+        index: true,
+        Component: AuthHome,
+      },
+      // 로그인 경로(path)와 Component 설정
+      {
+        path: "login",
+        Component: Login,
+      },
+      // 회원가입 경로(path)와 Component 설정
+      {
+        path: "signup",
+        Component: Signup,
+      },
+    ],
   },
 ]);
 
